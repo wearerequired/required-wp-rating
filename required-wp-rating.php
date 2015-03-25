c7e09b44b3cddc560087c43dcf9790dc87ef3f64
Pascal Birchler <hello@pascalbirchler.ch>
2015-03-25 10:55:50 +0100

    Remove unused code and assets

--- required-wp-rating.php
+++ required-wp-rating.php
@@ -36,13 +36,6 @@ if ( ! defined( 'WPINC' ) ) {
 
 require_once( plugin_dir_path( __FILE__ ) . 'public/RplusWpRating.php' );
 
-/*
- * Register hooks that are fired when the plugin is activated or deactivated.
- * When the plugin is deleted, the uninstall.php file is loaded.
- */
-register_activation_hook( __FILE__, array( 'RplusWpRating', 'activate' ) );
-register_deactivation_hook( __FILE__, array( 'RplusWpRating', 'deactivate' ) );
-
 /**
  * Initialize the plugins base class
  */
